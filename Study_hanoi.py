022cd1c43542f96faeffd7cdd1911a469bd52981
mtazo <groud.zero.mt@gmail.com>
2021-05-22 22:59:22 +0900

    fuga06

--- Study_hanoi.py
+++ Study_hanoi.py
@@ -16,24 +16,24 @@ def main():
 class Tower:
     """Represents a tower."""
 
-    def __init__(self, name, n_disk = 0):
+    def __init__(self, name, n_disks = 0):
         self.name = name
         self.disks = []
 
         for i in range(n_disks, 0, -1):
             self.push(str(i))
             
-    def push(self, disk):    
+    def push(self, disk):
         self.disks.append(disk)
 
     def pop(self):
-        return self,disks.pop()
+        return self.disks.pop()
 
     def __str__(self):
-        disk = ''.join('{:<2}'.format(d) for d in self.disks)
+        disks = ''.join('{:<2}'.format(d) for d in self.disks)
         return '{}: {}'.format(self.name, disks)
 
-class TowerOfHani:
+class TowerOfHanoi:
     """Represent the Tower of Hanoi's problem and solution."""
 
     def __init__(self, n_disk):
@@ -42,7 +42,7 @@ class TowerOfHani:
         self.prepare_towers()
     
     def prepare_towers(self):
-        s = Tower('s', n_disk)
+        s = Tower('s', self.n_disk)
         d = Tower('d')
         e = Tower('e')
 
@@ -50,7 +50,7 @@ class TowerOfHani:
 
 
     def run(self):
-        self.show_tower()
+        self.show_towers()
         s, d, e = self.towers
         self.move_disk(s, d, e, self.n_disk)
 
@@ -60,5 +60,19 @@ class TowerOfHani:
         
         self.move_disk(s, e, d, n -1)
 
-        
+        self.n_move += 1
+        print('move disk-{} form {} to {} ({}).'.format(str(n), s.name, d.name, self.n_move))
+        disk = s.pop()
+        d.push(disk)
+        self.show_towers()
+
+        self.move_disk(e, d, s, n - 1)
+
+    def show_towers(self):
+        for t in self.towers:
+            print(t)
+
+if __name__ == '__main__':
+    main()
+                
     
